f909734be11ceb574e71e798bf63a600bb861cb8
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2020-10-13 00:47:19 +0800

    update

--- application/views/based/nav.php
+++ application/views/based/nav.php
@@ -12,12 +12,12 @@
 								
 								</li>
 								<li aria-haspopup="true">
-									<a href="#" class="sub-icon">
+									<a href="<?php echo site_url('kontraktor') ?>" class="sub-icon">
 									<svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
 										Kontraktor <i class="fa fa-angle-down horizontal-icon"></i>
 									</a>
 									<ul class="sub-menu">
-										<li><a href="<?php echo site_url('kontraktor') ?>">Senarai Kontraktor</a></li>
+										<li><a href="<?php echo site_url('senarai-kontraktor') ?>">Senarai Kontraktor</a></li>
 										<li><a href="<?php echo site_url('maklumat-kontraktor/daerah')?>">Maklumat Syarikat</a></li>
 									</ul>
 								</li>
